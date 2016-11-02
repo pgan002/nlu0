f570b76ed50366ce2655af85bc2dc4dd25f67d9e
philip <phil.ganchev@gmail.com>
2016-11-02 14:46:38 -0700

    Code to get airport names and locations

---
+++ parse_airports.py
@@ -0,0 +1,41 @@
+import os
+import re
+
+import requests
+
+import lxml.html
+
+
+URL_TEMPLATE = 'http://en.wikipedia.org/wiki/List_of_airports_by_IATA_code:_'
+DATA_DIR = 'data'
+
+airport_name_re = re.compile(r'([A-Za-z ]+) (Domestic|Regional|International )?')
+
+category = 'Airport|Airfield|Airstrip|Aircraft Field|Field|Airpark|Aerodrome|Air Base|Air Force Base|Seaplane Base|RAF|Airbase|Air Park'
+
+data = []
+
+def get_and_parse_htmls():
+  global data
+  for char in range(ord('A'), ord('Z')):
+    url = URL_TEMPLATE + chr(char)
+    print url
+    response = requests.get(url)
+    doc = lxml.html.fromstring(response.content)
+    tables = doc.xpath('body/div/div/div/table')
+    assert len(tables) == 1
+    trs = tables[0].findall('tr')
+    for tr in trs:
+      tds = tr.findall('td')
+      if len(tds) >= 6:
+        row = [unicode(td.text_content()) for td in tds[:4]]  # iata, icao, name, location
+        data.append(row)
+  print len(data)
+
+
+def write_parsed_data():
+  with open(os.path.join(DATA_DIR, 'airports.csv'), 'w') as f:
+    for row in data:
+      row = '\t'.join(row).encode('utf8')
+      f.write(row + '\n')
+
